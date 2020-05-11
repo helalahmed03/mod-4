eb4376d3170f4b6eeaddcc81e696409197281f5a
helalahmed03 <54708950+helalahmed03@users.noreply.github.com>
2020-05-11 17:14:08 +0530

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -1,6 +1,6 @@
 (function(window) {
   // STEP 2: Wrap the entire contents of SpeakHello.js inside of an IIFE
-  // See Lecture 52, part 2
+  //See Lecture 52, part 2
 
 
   // STEP 3: Create an object, called 'helloSpeaker' to which you will attach
